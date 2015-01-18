cc19a14b121b768e8f673489aa4e46d1a8e5e084
HassenPy <hbt.youtube@yahoo.com>
2015-01-18 21:22:45 +0100

    created login and logout URLs

--- crmapp/urls.py
+++ crmapp/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from django.contrib.auth.views import login
 
 from marketing.views import HomePage
 from subscribers.views import subscriber_view
@@ -15,7 +14,8 @@ urlpatterns = patterns('',
                        # Subscriber related URLs
                        url(r'^signup/', subscriber_view, name='signup_url'),
                        # Login/Logout URLs
-                       url(r'^login/', login_view)
+                       url(r'^login/', 'django.contrib.auth.views.login', {'template_name': 'login.html'})
+                       url(r'^logout/', 'django.contrib.auth.views.logout', {'next_page': '/login/'})
                        # Account related URLs
                        # Contact related URLS
                        # Communication related URLs
