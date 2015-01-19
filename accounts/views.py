a36754e1eefa1ba8949e17ae385620954e7be880
HassenPy <hbt.youtube@yahoo.com>
2015-01-19 16:18:06 +0100

    added search functionality

--- accounts/views.py
+++ accounts/views.py
@@ -12,7 +12,16 @@ class AccountList(ListView):
     context_object_name = "accounts"
 
     def get_queryset(self):
-        account_list = Account.objects.filter(owner=self.request.user)
+        try:
+            a = self.request.GET.get('account',)
+        except KeyError:
+            a = None
+        if a:
+            account_list = Account.objects.filter(name__icontains=a,
+                                                  owner=self.request.user
+                                                  )
+        else:
+            account_list = Account.objects.filter(owner=self.request.user)
         return account_list
 
     @method_decorator(login_required)
