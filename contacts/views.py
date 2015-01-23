4d313ac62cdba85508c52deb40fa98d7df02b089
HassenPy <hbt.youtube@yahoo.com>
2015-01-23 11:58:31 +0100

    added contact detail view

--- contacts/views.py
+++ contacts/views.py
@@ -1,1 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+from .models import Contact
+
+
+@login_required()
+def contact_detail(request, uuid):
+
+    contact = Contact.objects.get(uuid=uuid)
+
+    return render(request,
+                  'contacts/contact_detail.html',
+                  {'contact': contact}
+                  )
