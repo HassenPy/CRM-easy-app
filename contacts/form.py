9eea8240e574ccedc3021f8c8e748ef9a48f57ba
HassenPy <hbt.youtube@yahoo.com>
2015-01-23 17:04:43 +0100

    created new account view

--- contacts/form.py
+++ contacts/form.py
@@ -3,7 +3,7 @@ from django import forms
 from .models import Contact
 
 
-class AccountForm(forms.ModelForm):
+class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = ('first_name', 'last_name', 'role',
--- contacts/views.py
+++ contacts/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseForbidden
 from django.contrib.auth.decorators import login_required
+from django.code.urlresolvers import reverse
 
 from .models import Contact
 from accounts.models import Account
+from .form import ContactForm
 
 
 @login_required
@@ -12,3 +14,27 @@ def contact_detail(request, uuid):
                   'contacts/contact_detail.html',
                   {'contacts': contact}
                   )
+
+
+@login_required
+def cont_creation(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            account = form.cleaned_data['account']
+
+            if account.owner != request.user:
+                return HttpResponseForbidden
+
+            contact = form.save(commit=False)
+            contact.owner = request.user
+            contact.save()
+
+            reverse_url = reverse('account_detail', args=(account.uuid,))
+            return redirect(reverse_url)
+    else:
+        form = AccountForm()
+
+    template_vars = {'form': form}
+    template = 'contacts/contact_creation.html'
+    return render(request, template, template_vars)
