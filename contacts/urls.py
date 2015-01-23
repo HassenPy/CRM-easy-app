5a440a9b8f168e0317d1750ce3c55f891ddffca2
HassenPy <hbt.youtube@yahoo.com>
2015-01-23 16:33:24 +0100

    created new account url

--- contacts/urls.py
+++ contacts/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, url
 
 contact_urls = patterns('',
+                        url('^new/$', 'contacts.views.cont_creation',
+                            name='contact_new'),
                         url('^$', 'contacts.views.contact_detail',
                             name="contact_detail")
                         )
