eef252177b58ecc2b395242cf63d758c356047ca
HassenPy <hbt.youtube@yahoo.com>
2015-01-27 16:49:47 +0100

    created the communication detail view

--- communications/views.py
+++ communications/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponseForbidden
+from django.contrib.auth.decorators import login_required
+# from django.core.urlresolvers import reverse
 
-# Create your views here.
+from .models import Communication
+
+
+@login_required
+def comm_detail(request, uuid):
+
+    comm = Communication.objects.get(uuid=uuid)
+    if comm.owner != request.user:
+            return HttpResponseForbidden()
+
+    return render(request, 'communications/comm_detail.html', {'comm': comm})
