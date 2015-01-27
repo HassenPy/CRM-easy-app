052e1b50e4c7ab3df1f2991ed0680698697831b1
HassenPy <hbt.youtube@yahoo.com>
2015-01-27 16:41:47 +0100

    created the communications model

--- communications/admin.py
+++ communications/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Communication
 
-# Register your models here.
+
+class ComList(admin.ModelAdmin):
+    list_display = ('subject', 'uuid')
+
+admin.site.register(Communication, ComList)
