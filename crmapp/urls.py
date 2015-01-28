b8f3af0ee76f065aaa3841411438c92134d0a06f
HassenPy <hbt.youtube@yahoo.com>
2015-01-28 11:16:27 +0100

    created the comm_creation url

--- crmapp/urls.py
+++ crmapp/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.auth.views import login, logout
 
 from marketing.views import HomePage
 from subscribers.views import subscriber_view
@@ -7,38 +8,62 @@ from accounts.urls import account_urls
 from contacts.urls import contact_urls
 from contacts.views import cont_creation, ContactDelete
 from communications.urls import com_urls
+from communications.views import comm_creation
 
 urlpatterns = patterns('',
 
                        # Admin URL
-                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'^admin/',
+                           include(admin.site.urls)
+                           ),
+
                        # Marketing pages
-                       url(r'^$', HomePage.as_view(), name="home"),
+                       url(r'^$',
+                           HomePage.as_view(),
+                           name="home"
+                           ),
+
                        # Subscriber related URLs
-                       url(r'^signup/', subscriber_view, name='signup_url'),
+                       url(r'^signup/',
+                           subscriber_view,
+                           name='signup_url'
+                           ),
+
                        # Login/Logout URLs
-                       url(r'^login/$', 'django.contrib.auth.views.login',
+                       url(r'^login/$',
+                           login,
                            {'template_name': 'login.html'}
                            ),
-                       url(r'^logout/$', 'django.contrib.auth.views.logout',
+                       url(r'^logout/$',
+                           logout,
                            {'next_page': '/login/'}
                            ),
+
                        # Account related URLs
-                       url(r'^account/', include(account_urls)),
+                       url(r'^account/',
+                           include(account_urls)
+                           ),
+
                        # Contact related URLS
-                       url(r'^contact/new/$', cont_creation,
+                       url(r'^contact/new/$',
+                           cont_creation,
                            name='contact_new'
                            ),
-                       url(r'^contact/(?P<uuid>[\w-]+)/',
-                           include(contact_urls)
+                       url(r'^contact/(?P<uuid>[\w-]+)/', include(contact_urls)
                            ),
 
                        url(r'^contact/(?P<pk>[\w-]+)/delete/$',
                            ContactDelete.as_view(),
                            name='contact_delete'
                            ),
+
                        # Communication related URLs
+                       url(r'^comm/new/$',
+                           comm_creation,
+                           name='comm_new'
+                           ),
                        url(r'^comm/(?P<uuid>[\w-]+)',
                            include(com_urls)
                            ),
+
                        )
