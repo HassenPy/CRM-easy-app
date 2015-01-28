b12fec378ad76cf2b3e7d67d31a4f71f1105534b
HassenPy <hbt.youtube@yahoo.com>
2015-01-28 10:00:46 +0100

    created communications form

---
+++ communications/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+
+from .models import Communication
+
+
+class ComForm(forms.Modelform):
+
+    model = Communication
+    fields = ('subject', 'notes', 'kind', 'date', 'account')
+    widgets = {
+        'subject': forms.TextInpt(
+            attrs={'placeholder': 'Subject',
+                   'class': 'form-control'
+                   }
+        ),
+        'notes': forms.Textarea(
+            attrs={'placeholder': 'Notes',
+                   'class': 'form-control'
+                   }
+        ),
+        'kind': forms.Select(
+            attrs={'placeholder': 'Type',
+                   'class': 'form-control'
+                   }
+        ),
+        'date': forms.DateInput(
+            attrs={'placeholder': 'Date',
+                   'class': 'form-control'
+                   }
+        ),
+    }
