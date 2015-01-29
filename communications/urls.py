a359c69955bb52d1f0d09c5a9256e08432d587bb
HassenPy <hbt.youtube@yahoo.com>
2015-01-29 12:39:48 +0100

    enabled communication edit feature in templates

--- communications/urls.py
+++ communications/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import patterns, url
-from .views import comm_details
+from .views import comm_details, comm_creation
 
 
 com_urls = patterns('',
                     url(r'^$', comm_details,
                         name='comm_detail'
                         ),
-                    url(r'^edit/$', comm_details,
+                    url(r'^edit/$', comm_creation,
                         name='comm_update'
                         ),
                     )
