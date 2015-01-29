dc0b6e647da729223890826467bad947b933e3c7
HassenPy <hbt.youtube@yahoo.com>
2015-01-29 12:29:00 +0100

    created the edit communication view

--- communications/views.py
+++ communications/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponseForbidden
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
@@ -6,6 +6,7 @@ from django.core.urlresolvers import reverse
 
 from .models import Communication
 from .forms import CommunicationForm
+from accounts.models import Account
 
 
 @login_required
@@ -18,15 +19,23 @@ def comm_details(request, uuid):
     return render(request, 'communications/comm_detail.html', {'comm': comm})
 
 
-@login_required()
-def comm_creation(request):
+@login_required
+def comm_creation(request, uuid=None, account=None):
+
+    if uuid:
+        comm = get_object_or_404(Communication, uuid=uuid)
+        if comm.owner != request.user:
+            return HttpResponseForbidden()
+    else:
+        comm = Communication(owner=request.user)
 
     if request.POST:
-        form = CommunicationForm(request.POST)
-        print form.errors
+        form = CommunicationForm(request.POST, instance=comm)
+
         if form.is_valid():
             # make sure the user owns the account
             account = form.cleaned_data['account']
+
             if account.owner != request.user:
                 return HttpResponseForbidden()
             # save the data
@@ -35,14 +44,21 @@ def comm_creation(request):
             comm.save()
             # return the user to the account detail view
             reverse_url = reverse('account_detail', args=(account.uuid,))
-            print 'redirecting'
             return redirect(reverse_url)
+        else:
+            account = form.cleaned_data['account']
+
     else:
-        print 'not valid form'
-        form = CommunicationForm()
+        form = CommunicationForm(instance=comm)
+    acc_id = request.GET.get('account', '')
+
+    if acc_id:
+        account = Account.objects.get(id=acc_id)
 
     template_vars = {
         'form': form,
+        'comm': comm,
+        'account': account
     }
 
     template_name = 'communications/comm_creation.html'
